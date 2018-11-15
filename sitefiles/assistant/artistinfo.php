02816475015dd25f2c2a937b69f3b3658dbbfd7f
Angela Berardinelli <aberardinelli@mercyhurst.edu>
2018-11-15 15:07:00 -0500

    Files added and updated from class Nov 15 2018

--- sitefiles/assistant/artistinfo.php
+++ sitefiles/assistant/artistinfo.php
@@ -7,17 +7,16 @@
 <body>
 <h1>FAME: Artist Management Enterprises</h1>
 
-<h2>Verify recent performances and send invoices</h2>
+<h2>View and Edit Artist Info / Contract Info</h2>
 
 <?php
 // database connection file is one folder up, use ../
 include '../dbconn.php';
 
-$query = "SELECT PerformanceID, PerformanceDate, Venue
-    FROM PERFORMANCE
-    WHERE PerformanceDate < CURRENT_DATE
-    AND Status != 'Cancelled'
-    AND InvoiceStatus != 'Paid';";
+$query = "SELECT ArtistID, MAX(EndDate) AS End
+    FROM CONTRACT 
+    GROUP BY ArtistID
+    ORDER BY End;";
 
 $result = mysqli_query($conn,$query);
 
@@ -26,24 +25,28 @@ if (mysqli_error($conn))
     
 // IF the query returned any results...
 if (mysqli_num_rows($result) > 0) {
-    echo "<p>All events that still need to be invoiced:</p>";
+    echo "<p>All artist contract records:</p>";
     
     // First create a table, with column headings
     echo "<table>";
-    echo "<tr><th>Performance ID</th><th>Performance Date</th><th>Venue ID</th><th>Click button to take action</th></tr>";
+    echo "<tr><th>Artist ID</th><th>Contract End Date</th><th>Click button to take action</th></tr>";
     
     // Then loop through the output of the query, creating a row
     // in the HTML table for each row in $result
     while($row = mysqli_fetch_assoc($result)) {
-        echo "<tr><td>".$row["PerformanceID"]."</td><td>".
-            $row["PerformanceDate"]."</td><td>".
-            $row["Venue"]."</td></tr>";
+        echo "<tr><td>".$row["ArtistID"]."</td><td>".
+            $row["End"]."</td><td>".
+            "Buttons go here</td></tr>";
+    // ON TUESDAY BEFORE THANKSGIVING:
+    // 1. ADD THE BUTTONS HERE
+    // 2. How to make manager/assistant only see THEIR artist clients?
+    // 3. (Optional) Conditional buttons based on status of contract
     }
     // After you're done adding the rows, end the table.
     echo "</table>";
 } // end of what we do IF the query returned results
 // OTHERWISE tell the user there were no results
-else {    echo "No invoices need to be generated at this time.";   }
+else {    echo "No artist contracts in database.";   }
 
 
 // Always remember to close the connection to the database when you're done!
